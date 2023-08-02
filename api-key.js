65580a1cf4889ae295da28e5fe19190d77f73c37
interactivepix <122866945+interactivepix@users.noreply.github.com>
2023-08-02 08:12:34 +0100

    Commit

---
+++ api-key.js
@@ -0,0 +1,5 @@
+// YOUR KEY HERE TO MAKE IT WORK, OTHERWISE YOU WILL GET ERROR:
+// Uncaught ReferenceError: apikey is not defined
+//     at script.js:7:28
+// Get it here: https://api.api-ninjas.com/v1/loremipsum?paragraphs="
+let apiKey = "";
